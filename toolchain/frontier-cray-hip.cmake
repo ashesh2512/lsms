949cedd563437d5e16c527d764c3ec121cfcfa88
Ashesh <ashesh.sharma@hpe.com>
2025-05-14 15:59:27 -0500

    modify cray cmake file to be consistent with other frontier cmake files

--- toolchain/frontier-cray-hip.cmake
+++ toolchain/frontier-cray-hip.cmake
@@ -7,6 +7,9 @@ set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF)
 
 set(SEARCH_LAPACK OFF)
 set(SEARCH_BLAS OFF)
+
+set(BUILD_WITH_OPENMP OFF)
+
 set(BLAS_LIBRARIES "$ENV{CRAY_LIBSCI_PREFIX_DIR}/lib/libsci_cray.a")
 set(LAPACK_LIBRARIES "$ENV{CRAY_LIBSCI_PREFIX_DIR}/lib/libsci_cray.a")
 set(USE_ACCELERATOR_HIP ON)
@@ -16,7 +19,7 @@ set(MST_BUILD_KKR_MATRIX_DEFAULT 0x3000)
 #set(MST_BUILD_KKR_MATRIX_DEFAULT 0x1000)
 set(AMDGPU_TARGETS "gfx90a")
 set(GPU_TARGETS "gfx90a")
-set(CMAKE_CXX_FLAGS "-std=c++17 --gcc-toolchain=/opt/cray/pe/gcc/12.2.0/snos -L/opt/cray/pe/gcc/12.2.0/snos/lib64 -mtune=native")
+#set(CMAKE_CXX_FLAGS "-std=c++17 --gcc-toolchain=/opt/cray/pe/gcc/12.2.0/snos -L/opt/cray/pe/gcc/12.2.0/snos/lib64 -mtune=native")
 
 # Loaded Modules:
 # module load PrgEnv-cray-amd
